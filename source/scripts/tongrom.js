c117331a110d6fbe5c72e4af7c8b01571999be47
Julian Konrath <julian.konrath@gmail.com>
2022-03-05 15:37:29 +0100

    disallow intersection secants

--- source/scripts/tongrom.js
+++ source/scripts/tongrom.js
@@ -1,4 +1,4 @@
-function createDot(center, color) {
+function drawDot(center, color) {
   new paper.Path.Circle({
     center: center,
     radius: 5,
@@ -13,8 +13,11 @@ function getRandomPointOn(shape) {
 function drawSecantOn(shape) {
   var secant = new paper.Path();
   secant.strokeColor = '#222';
-  secant.add(getRandomPointOn(shape));
-  secant.add(getRandomPointOn(shape));
+  var start = getRandomPointOn(shape);
+  var end = getRandomPointOn(shape);
+  secant.add(start);
+  secant.add(end);
+  return secant;
 }
 
 // Create a circle shaped path, which is automatically
@@ -33,7 +36,30 @@ var circle = new paper.Path.Circle({
 //   createDot(intersections[i].point);
 // }
 
-for (var i = 0; i < 4; i++) {
-  console.log('drawing secant');
-  drawSecantOn(circle);
+var secants = [];
+
+var i = 0;
+for (; i < 2; i++) {
+  console.log('drawing secant', 'i=', i);
+  var newSecant = drawSecantOn(circle);
+
+  var secantHasIntersections = false;
+  for (var j = 0; j < secants.length; j++) {
+    var intersections = newSecant.getIntersections(secants[j]);
+    if (intersections.length) {
+      console.log('secant has intersections');
+      newSecant.remove();
+      i = i - 1;
+      secantHasIntersections = true;
+      break;
+    }
+  }
+  if (secantHasIntersections) {
+    continue;
+  }
+  console.log(newSecant.segments);
+  for (var k = 0; k < newSecant.segments.length; k++) {
+    drawDot(newSecant.segments[k].point);
+  }
+  secants.push(newSecant);
 }
